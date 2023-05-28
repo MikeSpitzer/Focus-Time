0a6260541c1744b1b30caf7a6d77223472baaf90
Michael Spitzer <dawgofwar@gmail.com>
2023-05-27 22:01:30 -0500

    Adaption and testing of features to focus.js

--- src/features/focus/Focus.js
+++ src/features/focus/Focus.js
@@ -1,31 +1,91 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 import { TextInput } from 'react-native-paper';
 import { RoundedButton } from '../../components/RoundedButton';
 import { fontSizes, spacing } from '../../utils/sizes';
 import { colors } from '../../utils/colors';
+import { Alert } from 'react-native';
 
 export const Focus = ({ addSubject }) => {
   const [subject, setSubject] = useState(null);
+  const [goals, setGoals] = useState([]);
+
+  const addGoal = (goal, points = 10) => {
+    setGoals([...goals, { goal, points }]);
+  };
+
+  const resetGoals = () => {
+    Alert.alert(
+      'Confirm Reset',
+      'Are you sure you want to reset the list of goals?',
+      [
+        {
+          text: 'Cancel',
+          style: 'cancel',
+        },
+        {
+          text: 'Reset',
+          onPress: () => {
+            setGoals([]);
+          },
+        },
+      ],
+      { cancelable: false }
+    );
+  };
+
+  const totalPoints = goals.reduce((total, goal) => total + goal.points, 0);
+
+  const renderItem = ({ item, index }) => (
+    <View style={styles.goalItem}>
+      <Text style={styles.goalText}>{item.goal}</Text>
+      <TouchableOpacity
+        style={styles.pointsButton}
+        onPress={() => {
+          const newPoints = prompt('Enter new points:', item.points.toString());
+          if (newPoints !== null) {
+            const updatedGoals = goals.map((g, i) => {
+              if (i === index) {
+                return { ...g, points: parseInt(newPoints) };
+              } else {
+                return g;
+              }
+            });
+            setGoals(updatedGoals);
+          }
+        }}
+      >
+        <Text style={styles.pointsText}>{item.points} points</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <View style={styles.innerContainer}>
-        <Text style={styles.title}>What would you like to focus on?</Text>
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={{ flex: 1, marginRight: spacing.md }}
-            onSubmitEditing={({ nativeEvent }) => {
-              setSubject(nativeEvent.text)
-            }}
-          />
-          <RoundedButton
-            size={50}
-            title="+"
-            onPress={() => {
-              addSubject(subject);
-            }}
-          />
-        </View>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Today I will focus on:</Text>
+        <TextInput
+          style={styles.subjectInput}
+          placeholder="Enter a subject"
+          value={subject}
+          onChangeText={setSubject}
+        />
+        <RoundedButton
+          title="Add Goal"
+          onPress={() => {
+            addSubject(subject);
+            setSubject(null);
+          }}
+        />
+        <RoundedButton title="Reset Goals" onPress={resetGoals} />
+      </View>
+      <View style={styles.body}>
+        <FlatList
+          data={goals}
+          renderItem={renderItem}
+          keyExtractor={(item, index) => index.toString()}
+        />
+        <Text style={styles.totalPoints}>Total Points: {totalPoints}</Text>
       </View>
     </View>
   );
@@ -37,17 +97,65 @@ const styles = StyleSheet.create({
   },
   innerContainer: {
     flex: 1,
-    padding: spacing.md,
+    alignItems: 'center',
     justifyContent: 'center',
   },
   title: {
-    color: colors.white,
-    fontWeight: 'bold',
     fontSize: fontSizes.lg,
+    color: colors.white,
   },
   inputContainer: {
-    paddingTop: spacing.md,
     flexDirection: 'row',
     alignItems: 'center',
+    marginTop: spacing.md,
+  },
+  list: {
+    marginTop: spacing.md,
+    marginBottom: spacing.md,
+  },
+  goalItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: spacing.sm,
+  },
+  goalText: {
+    flex: 1,
+    color: colors.white,
+    fontSize: fontSizes.md,
+  },
+  pointsButton: {
+    backgroundColor: colors.blue,
+    padding: spacing.sm,
+    borderRadius: 5,
+    marginRight: spacing.sm,
+  },
+  pointsText: {
+    color: colors.white,
+    fontSize: fontSizes.md,
+  },
+  deleteButton: {
+    backgroundColor: colors.red,
+    padding: spacing.sm,
+    borderRadius: 5,
+  },
+  deleteButtonText: {
+    color: colors.white,
+    fontSize: fontSizes.md,
+    fontWeight: 'bold',
+  },
+  totalPoints: {
+    color: colors.white,
+    fontSize: fontSizes.md,
+    fontWeight: 'bold',
+  },
+  resetButton: {
+    backgroundColor: colors.red,
+    padding: spacing.md,
+    borderRadius: 5,
+  },
+  resetButtonText: {
+    color: colors.white,
+    fontSize: fontSizes.md,
+    fontWeight: 'bold',
   },
 });
